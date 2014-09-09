c6a2ff2797a7690d55b241f66ae9a2549d0df6c6
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2014-09-10 02:07:39 +0700

    Implement Test model/query (default findAll)
    * Also custom reflectPhpDoc()

---
+++ src/Model/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @method integer getId() getId() Gets the current value of id
+ * @method WpTesting_Model_Test setId() setId(integer $id) Sets the value for id
+ * @method string getTitle() getTitle() Gets the current value of title
+ * @method WpTesting_Model_Test setTitle() setTitle(string $title) Sets the value for title
+ * @method fTimestamp getCreated() getCreated() Gets the current value of created
+ * @method WpTesting_Model_Test setCreated() setCreated(fTimestamp|string $created) Sets the value for created
+ * @method fTimestamp getModified() getModified() Gets the current value of modified
+ * @method WpTesting_Model_Test setModified() setModified(fTimestamp|string $modified) Sets the value for modified
+ */
+class WpTesting_Model_Test extends WpTesting_Model_AbstractModel
+{
+
+}
