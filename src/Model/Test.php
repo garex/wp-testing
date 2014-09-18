98866ec66974ac0e5bf02a98738c11eb7ea1ae00
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2014-09-18 10:29:40 +0700

    Get scales by test

--- src/Model/Test.php
+++ src/Model/Test.php
@@ -18,6 +18,11 @@ class WpTesting_Model_Test extends WpTesting_Model_AbstractModel
         'modified'  => 'post_modified',
     );
 
+    /**
+     * @var WpTesting_Model_Taxonomy[]
+     */
+    protected $taxonomies = null;
+
     public function __construct($key = null)
     {
         if ($key instanceof WP_Post) {
@@ -32,11 +37,55 @@ class WpTesting_Model_Test extends WpTesting_Model_AbstractModel
     }
 
     /**
-     * @return WpTesting_Model_Question
+     * @return WpTesting_Model_Question[]
      */
     public function buildQuestions()
     {
         return $this->buildWpTesting_Model_Questions();
+    }
+
+    /**
+     * @return WpTesting_Model_Scale[]
+     */
+    public function buildScales()
+    {
+        return fRecordSet::build('WpTesting_Model_Scale', array(
+            'term_id=' => $this->getTermIdFromFilteredTaxonomies('wpt_scale'),
+        ));
+    }
+
+    /**
+     * @return WpTesting_Model_Taxonomy[]
+     */
+    protected function buildTaxonomies()
+    {
+        return $this->buildWpTesting_Model_Taxonomy();
+    }
+
+    /**
+     * @param strnig $taxonomyFilter
+     * @return fRecordSet of WpTesting_Model_Taxonomy
+     */
+    protected function buildTaxonomiesOnce()
+    {
+        if (is_null($this->taxonomies)) {
+            $this->taxonomies = $this->buildTaxonomies();
+        }
+        return $this->taxonomies;
+    }
+
+    /**
+     * Filter related taxonomies and return term id from they
+     * @param string $taxonomy One of wpt_answer|wpt_scale|wpt_result|wpt_category
+     * @return array
+     */
+    protected function getTermIdFromFilteredTaxonomies($taxonomy)
+    {
+        $ids = array();
+        foreach ($this->buildTaxonomiesOnce()->filter(array('getTaxonomy=' => $taxonomy)) as $taxonomy) {
+            $ids[] = $taxonomy->getTermId();
+        }
+        return $ids;
     }
 
     public function getQuestionsPrefix()
