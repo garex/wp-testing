9f963d1c2c4b5d6731be8de4be8b8ebfcd0633cd
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2014-10-04 02:50:35 +0700

    Add basic formula parser with test

---
+++ src/Model/Formula.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * Formula that translates passed values into true or false
+ *
+ * Formula text and values are immutable — can be set only once.
+ *
+ * Values with percents are replaced for their percentage analogs (when source contains %).
+ *
+ */
+class WpTesting_Model_Formula
+{
+
+    /**
+     * Formula's source — some kind of "source code"
+     * @var string
+     */
+    private $source = null;
+
+    /**
+     * Values, that are substitutes in formula during comparing
+     * @var array
+     */
+    private $values = array();
+
+    /**
+     * @param string $source Formula's source — some kind of "source code"
+     */
+    public function __construct($source)
+    {
+        $this->setSource($source);
+    }
+
+    /**
+     * Adds value to values list without rewriting. Converts value type to integer if it's not double.
+     *
+     * @param string $name
+     * @param numeric $value
+     * @param float $percentageValue Required only when we have "%" in source
+     * @throws InvalidArgumentException
+     * @return WpTesting_Model_Formula
+     */
+    public function addValue($name, $value, $percentageValue = null)
+    {
+        if (isset($this->values[$name])) {
+            throw new InvalidArgumentException('Value ' . $name . ' can not be added twice');
+        }
+        if (is_null($value)) {
+            throw new InvalidArgumentException('Value ' . $name . ' can not be null');
+        }
+
+        if (strpos($this->source, '%')) {
+            if (is_null($percentageValue)) {
+                throw new InvalidArgumentException('Percentage value ' . $name . ' can not be null when source contains percentage');
+            }
+            if (!is_numeric($percentageValue)) {
+                throw new InvalidArgumentException('Percentage value ' . $name . ' must be numeric. Provided: ' . var_export($percentageValue, true));
+            }
+            $this->values[$name] = floatval($percentageValue);
+            return $this;
+        }
+
+        if (!is_numeric($value)) {
+            throw new InvalidArgumentException('Value ' . $name . ' must be numeric. Provided: ' . var_export($value, true));
+        }
+
+        $intValue   = intval($value);
+        $floatValue = floatval($value);
+        $this->values[$name] = ($intValue == $floatValue) ? $intValue : $floatValue;
+
+        return $this;
+    }
+
+    /**
+     * Evaluates formula with values and checks if it's true or not.
+     *
+     * Currently most dangerous part.
+     *
+     * @return boolean
+     */
+    public function isTrue()
+    {
+        $result = @eval('return ' . $this->substitute() . ';');
+        return $result;
+    }
+
+    /**
+     * Substitutes values inside formula. Cleans up source from forbidden content.
+     *
+     * @throws PHPParser_Error
+     * @return string
+     */
+    public function substitute()
+    {
+        $result = $this->source;
+
+        // Replace all values
+        $result = str_replace(array_keys($this->values), array_values($this->values), $result);
+
+        // Lowercase
+        $result = strtolower($result);
+
+        // Replace and/or
+        $result = str_replace(array('and', 'or'), array('&&', '||'), $result);
+
+        // Leave only allowed
+        $result = preg_replace('/[^\d\-%<>=\(\)&\| \.]+/', '', $result);
+
+        // Normalize comparisions
+        $result = str_replace(array('><', '<>', '=>', '=<'), array('!=', '!=', '>=', '<='), $result);
+
+        // Normalize percents
+        $result = preg_replace('/%+/', '%', $result);
+
+        // Convert percents into floats
+        $result = preg_replace_callback('/(\d+) ?%/', array($this, 'transformPercent'), $result);
+
+        // Remove empty brackets
+        $result = preg_replace('/\( *\)/', '', $result);
+
+        // Remove percents without numbers
+        $result = preg_replace('/([^\d])%+/', '$1', $result);
+
+        // Normalize whitespaces
+        $result = preg_replace('/ +/', ' ', trim($result));
+
+        // Remove whitespaces around operators
+        $result = preg_replace('/ *([<>!=&\|]+) */', '$1', $result);
+
+        // Replace left whitespaces with ands
+        $result = preg_replace('/ +/', '&&', $result);
+
+        // Check if there is no parse error
+        $parser = new PHPParser_Parser(new PHPParser_Lexer());
+        $parser->parse('<?php ' . $result . ';');
+
+        return $result;
+    }
+
+    protected function transformPercent($matches)
+    {
+        return $matches[1] / 100;
+    }
+
+    /**
+     * Formula's source — some kind of "source code"
+     *
+     * @param string $source
+     * @throws InvalidArgumentException
+     * @return WpTesting_Model_Formula
+     */
+    protected function setSource($source)
+    {
+        if (is_null($source)) {
+            throw new InvalidArgumentException('Formula source can not be null');
+        }
+        if (!is_null($this->source)) {
+            throw new InvalidArgumentException('Formula source can not be rewritten');
+        }
+        $this->source = $source;
+        return $this;
+    }
+}
--- tests/Model/FormulaTest.php
+++ tests/Model/FormulaTest.php
@@ -3,9 +3,96 @@
 class FormulaTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testBasic()
+    public function testSlugsAndNamesTranslatedIntoValues()
     {
-        $this->markTestIncomplete();
+        $formula = new WpTesting_Model_Formula('Introversion/Extraversion scale > 12 and slug-neurothism < 12');
+        $formula->addValue('Introversion/Extraversion scale', 12)->addValue('slug-neurothism', 12);
+
+        $this->assertEquals('12>12&&12<12', $formula->substitute());
     }
 
+    public function testOnlyNumbersAllowedAsRealValuesOfVars()
+    {
+        $this->setExpectedException('InvalidArgumentException', 'must be numeric');
+        $formula = new WpTesting_Model_Formula('-');
+        $formula->addValue('key', 'value');
+    }
+
+    /**
+     * @dataProvider onlyAllowedSymbolsLeftProvider
+     * @param string $passedFormula
+     * @param string $expectedResult
+     */
+    public function testOnlyAllowedSymbolsLeft($passedFormula, $expectedResult)
+    {
+        $formula = new WpTesting_Model_Formula($passedFormula);
+        $this->assertEquals($expectedResult, $formula->substitute());
+    }
+
+    public function onlyAllowedSymbolsLeftProvider()
+    {
+        return array(
+            array('45 > 123 +*/\\  ;"!@#$%^*() hey! and 34 > 23 %', '45>123&&34>0.23'),
+            array('45 > 123 and 34 > 23',  '45>123&&34>23'),
+            array('somefunction(45) < 23', '(45)<23'),
+        );
+    }
+
+    /**
+     * @dataProvider operatorsNormalizedProvider
+     * @param string $passedFormula
+     * @param string $expectedResult
+     */
+    public function testOperatorsNormalized($passedFormula, $expectedResult)
+    {
+        $formula = new WpTesting_Model_Formula($passedFormula);
+        $this->assertEquals($expectedResult, $formula->substitute());
+    }
+
+    public function operatorsNormalizedProvider()
+    {
+        return array(
+            array('12 <> 12', '12!=12'),
+            array('12 >< 12', '12!=12'),
+
+            array('12 >= 12', '12>=12'),
+            array('12 <= 12', '12<=12'),
+            array('12 => 12', '12>=12'),
+            array('12 =< 12', '12<=12'),
+        );
+    }
+
+    /**
+     * @dataProvider logicalAndsPlacedAutomaticallyProvider
+     * @param string $passedFormula
+     * @param string $expectedResult
+     */
+    public function testLogicalAndsPlacedAutomatically($passedFormula, $expectedResult)
+    {
+        $formula = new WpTesting_Model_Formula($passedFormula);
+        $this->assertEquals($expectedResult, $formula->substitute());
+    }
+
+    public function logicalAndsPlacedAutomaticallyProvider()
+    {
+        return array(
+            array('12 > 12 and 23 < 45', '12>12&&23<45'),
+            array('12 > 12  23 < 45', '12>12&&23<45'),
+        );
+    }
+
+    public function testRequirePercentage()
+    {
+        $this->setExpectedException('InvalidArgumentException', 'can not be null when source contains percentage');
+        $formula = new WpTesting_Model_Formula('something > 50%');
+        $formula->addValue('something', '23');
+    }
+
+    public function testValuesReplacedByPercentageInPercentageFormulas()
+    {
+        $formula = new WpTesting_Model_Formula('something > 50%');
+        $formula->addValue('something', '23', 0.46);
+
+        $this->assertEquals('0.46>0.5', $formula->substitute());
+    }
 }
