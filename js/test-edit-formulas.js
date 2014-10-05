fcb0ae3f203cbbbc9bd06d3f0a199f91f27bfe92
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2014-10-05 13:34:06 +0700

    Fix formula editor by adding input type

--- js/test-edit-formulas.js
+++ js/test-edit-formulas.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     var box             = $('#wpt_edit_formulas'),
-        formulas        = box.find('.wpt_formulas input'),
+        formulas        = box.find('.wpt_formulas input[type=text]'),
         activeFormula   = formulas.length > 0 ? $(formulas[0]) : $('<input/>'),
         helpers         = box.find('.wpt_formulas_helper input[type=button]');
 
