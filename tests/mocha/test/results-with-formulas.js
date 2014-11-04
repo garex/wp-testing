ea447d2e67c73862358a105c4978834cb1204089
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2014-11-04 13:06:25 +0700

    Test results with formulas

--- tests/mocha/test/results-with-formulas.js
+++ tests/mocha/test/results-with-formulas.js
@@ -12,11 +12,63 @@ describe('Results with formulas', function() {
     })
 
     it('should be added to test and saved (even empty)', function() {
+        casper.then(function() {
+            this.clickLabel('Add New', '*[@id="menu-posts-wpt_test"]/*//a')
+        })
+
+        casper.then(function() {
+            'Fatal'.should.not.be.textInDOM
+            'Add New Test'.should.be.inTitle
+
+            this.fill('form#post', {
+                'post_title': 'Test With Results',
+                'wp_testing_model_questions::question_title[0]': 'Question 1?',
+                'wp_testing_model_questions::question_title[1]': 'Question 2?',
+                'wp_testing_model_questions::question_title[2]': 'Question 3?'
+            })
+            this.clickLabel(' Yes',         'label')
+            this.clickLabel(' Lie',         'label')
+            this.clickLabel(' Choleric',    'label')
+            this.clickLabel(' Melancholic', 'label')
+            this.click('#publish')
+        })
+
+        casper.waitForUrl(/message/, function() {
+            'Fatal'.should.not.be.textInDOM
+            '#message'.should.be.inDOM
+            '#wpt_edit_formulas .wpt_result'.should.be.inDOM
+        })
     })
 
     it('should be saved when formulas is good', function() {
+        casper.then(function() {
+            this.click('.wpt_formulas_helper input[data-source="scale-lie"]')
+            this.click('.wpt_formulas_helper input[data-source=">"]')
+            this.sendKeys('input[name="wp_testing_model_formulas::formula_source[0]"]', '10');
+            this.click('#publish')
+        })
+
+        casper.waitForUrl(/message/, function() {
+            'Fatal'.should.not.be.textInDOM
+            '#message'.should.be.inDOM
+            '#wpt_edit_formulas .wpt_result'.should.be.inDOM
+            expect('wp_testing_model_formulas::formula_source[0]').to.have.fieldValue('scale-lie > 10')
+        })
     })
 
     it('should error when formulas is bad', function() {
+        casper.then(function() {
+            this.click('.wpt_formulas_helper input[data-source="scale-lie"]')
+            this.click('.wpt_formulas_helper input[data-source="<="]')
+            this.sendKeys('input[name="wp_testing_model_formulas::formula_source[1]"]', 'nothing');
+            this.click('#publish')
+        })
+
+        casper.waitForUrl(/post/, function() {
+            'Fatal'.should.not.be.textInDOM
+            'Test data not saved'.should.be.textInDOM
+            'Formula for Melancholic has error'.should.be.textInDOM
+            this.clickLabel('« Back')
+        })
     })
 })
