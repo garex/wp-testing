71eb93213b35249781c53a049361ff131140a6fa
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-02-01 23:09:35 +0700

    Revert "Composer incompatibility workaround"
    This reverts commit d190e71f8916c3f23df488df1ec4ec7f59a1304c.

--- tests/integration-environment/composer-update.sh
+++ tests/integration-environment/composer-update.sh
@@ -5,10 +5,10 @@ source /etc/profile.d/phpenv.sh
 if [[ "$TRAVIS_PHP_VERSION" == "5.2" ]];
 then
     phpenv shell 5.3
-    composer selfupdate 1.0.0-alpha9
+    composer self-update
     composer update --no-interaction
     phpenv shell --unset
 else
-    composer selfupdate 1.0.0-alpha9
+    composer self-update
     composer update --no-interaction
 fi
