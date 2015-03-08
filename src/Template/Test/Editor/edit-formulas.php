4c837984592104d2fca46871dde3ca68eb8a8f9c
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-03-08 22:36:13 +0700

    Add more math operators: "+*/"
    * Update locale for "Operators"
    * Fix it under 5.2 @see https://bugs.php.net/bug.php?id=47229

--- src/Template/Test/Editor/edit-formulas.php
+++ src/Template/Test/Editor/edit-formulas.php
@@ -37,6 +37,7 @@
     <tr>
         <th class="bar"><?php echo __('Variables', 'wp-testing') ?></th>
         <th class="bar"><?php echo __('Comparisions', 'wp-testing') ?></th>
+        <th class="bar"><?php echo __('Operators', 'wp-testing') ?></th>
     </tr>
     <tr>
         <td>
@@ -44,15 +45,20 @@
             <input type="button" data-source="<?php echo htmlspecialchars($variable->getSource()) ?>" title="<?php echo $variable->getTypeLabel() ?>" value="<?php echo htmlspecialchars($variable->getTitle()) ?>"/>
         <?php endforeach ?>
         </td>
-        <td>
+        <td class="operators">
         <?php foreach(explode(', ', '<, >, <=, =>, <>, AND, OR, (, )') as $operator):  ?>
             <input type="button" data-source="<?php echo htmlspecialchars($operator) ?>" title="<?php echo __('Comparision', 'wp-testing') ?>" value="<?php echo htmlspecialchars($operator) ?>"/>
+        <?php endforeach ?>
+        </td>
+        <td class="operators">
+        <?php foreach(explode(', ', '+, -, *, /') as $operator):  ?>
+            <input type="button" data-source="<?php echo htmlspecialchars($operator) ?>" title="<?php echo __('Operator', 'wp-testing') ?>" value="<?php echo htmlspecialchars($operator) ?>"/>
         <?php endforeach ?>
         </td>
     </tr>
 <?php if (!count($variables)): ?>
     <tr class="alternate">
-        <td colspan="2">
+        <td colspan="3">
             <p class="highlight">
                 <?php echo __('No variables for formulas available. To use variables you must have scales selected.', 'wp-testing') ?>
             </p>
@@ -60,7 +66,7 @@
     </tr>
 <?php endif ?>
     <tr class="alternate">
-        <td colspan="2">
+        <td colspan="3">
             <div class="howto"><?php
             /* translators: "scale-bla" should not ne translated */
             echo __('Both numbers and percents allowed. For example "scale-bla" has total 30, then "scale-bla > 15" and "scale-bla > 50%" are same.', 'wp-testing');
