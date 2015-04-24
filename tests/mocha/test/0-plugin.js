93586337d9d353041aa5262fc0ee78e5f1f73e4f
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-04-25 03:23:18 +0600

    Fix plugins activation order

--- tests/mocha/test/0-plugin.js
+++ tests/mocha/test/0-plugin.js
@@ -11,33 +11,31 @@ describe('Plugin_activation', function() {
         })
     })
 
-    it('should be activated', function() {
+    var hasJetpack = false
+    it('should open plugins page', function() {
         casper.thenOpen('http://wpti.dev/wp-admin/plugins.php', function () {
             expect(/Plugins/).to.matchTitle
             'Wp-testing'.should.be.textInDOM
-            this.click('#cb input')
-            '#wpbody-content .wrap form'.should.be.inDOM
-            this.evaluate(function() {
-                jQuery('.wrap form select:first').val('activate-selected')
+            hasJetpack = this.evaluate(function() {
+                return window['jetpack-by-wordpress-com'] != undefined
             })
-            this.click('#doaction')
+        })
+    })
+
+    it('should activate jetpack plugin if it exists', function() {
+        if (!hasJetpack) {
+            this.skip()
+            return
+        }
+
+        casper.then(function () {
+            this.clickLabel('Activate', '*[@id="jetpack-by-wordpress-com"]/*//a')
         })
 
         casper.waitForUrl(/activate/, function() {
-            'Fatal'.should.not.be.textInDOM
-            '#wp-testing .deactivate a'.should.be.inDOM
+            '#toplevel_page_jetpack'.should.be.inDOM
+        })
 
-            var hasJetpack = this.evaluate(function() {
-                return jQuery('#toplevel_page_jetpack').length > 0
-            })
-
-            if (hasJetpack) {
-                activateJetpackModules()
-            }
-        }, null, 60000)
-    })
-
-    function activateJetpackModules() {
         casper.thenOpen('http://wpti.dev/wp-admin/admin.php?page=jetpack_modules', function () {
             expect(/Jetpack/).to.matchTitle
             this.click('.manage-left input.checkall')
@@ -50,6 +48,26 @@ describe('Plugin_activation', function() {
         casper.waitForSelector('.jetpack-module.active', function() {
             'Fatal'.should.not.be.textInDOM
         }, null, 60000)
-    }
 
+        casper.thenOpen('http://wpti.dev/wp-admin/plugins.php', function () {
+            expect(/Plugins/).to.matchTitle
+            'Wp-testing'.should.be.textInDOM
+        })
+    })
+
+    it('should activate main plugin and others', function() {
+        casper.then(function () {
+            this.click('#cb input')
+            '#wpbody-content .wrap form'.should.be.inDOM
+            this.evaluate(function() {
+                jQuery('.wrap form select:first').val('activate-selected')
+            })
+            this.click('#doaction')
+        })
+
+        casper.waitForUrl(/activate/, function() {
+            'Fatal'.should.not.be.textInDOM
+            '#wp-testing .deactivate a'.should.be.inDOM
+        }, null, 60000)
+    })
 })
