c52466193c625d970ecf5f0da5967e3695087b2b
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-07-08 23:47:29 +0600

    Clean up vendor directory for tests and bad files
    * Update composer
    * Add cleanup vendor script
    * Remove tests and bad assets from vendor

---
+++ tools/deploy/clean-up-vendor.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd vendor
+rm --force --recursive ruckusing/ruckusing-migrations/tests
+rm --force --recursive nikic/php-parser/grammar
+rm --force --recursive nikic/php-parser/test
+rm --force --recursive nikic/php-parser/test_old
+rm --force --recursive samyk/evercookie/assets/evercookie_sl
+rm --force             samyk/evercookie/assets/evercookie.jar
+rm --force             samyk/evercookie/assets/evercookie.jnlp
+rm --force             samyk/evercookie/assets/*.java
+rm --force --recursive pnegri/uuid-js/test
