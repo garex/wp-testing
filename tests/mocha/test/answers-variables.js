5074b114cef74d4c8aa106cc150ee8cd96674803
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-07-09 12:18:40 +0600

    Add questions/answers variables test

---
+++ tests/mocha/test/answers-variables.js
@@ -0,0 +1,66 @@
+describe('Answers variables', function() {
+
+    before(function () {
+        require('../login-as').admin(this)
+    })
+
+    var testEditUrl =
+        testViewUrl = ''
+
+    it('should show empty test as under construction initially', function() {
+        casper.thenOpen('http://wpti.dev/wp-admin/edit.php?post_type=wpt_test', function() {
+            this.clickLabel('Test With Answers Sorted', '*[@id="posts-filter"]/*//a')
+        })
+
+        casper.waitForUrl(/edit/, function() {
+            testEditUrl = this.getCurrentUrl()
+            this.evaluate(function() {
+                document.location = jQuery('#post-preview').attr('href')
+            })
+        })
+
+        casper.waitForUrl(/answers/, function() {
+            'Test is under construction'.should.be.textInDOM
+            '#wpt-test-form input[type=submit]'.should.not.be.inDOM
+            testViewUrl = this.getCurrentUrl()
+        })
+    })
+
+    it('should link to test one  result', function() {
+        casper.thenOpen(testEditUrl, function() {
+            this.clickLabel(' Choleric', 'label')
+            this.click('#publish')
+        })
+
+        casper.waitForUrl(/message/, function() {
+            'Fatal'.should.not.be.textInDOM
+            '#message'.should.be.inDOM
+        })
+    })
+
+    it('should add to test question-answer formula', function() {
+        casper.then(function() {
+            this.click('.wpt_formulas_helper button.question-answer')
+            this.click('#publish')
+        })
+    })
+
+    it('should show test as final then', function() {
+        casper.open(testViewUrl, function() {
+            'Test is under construction'.should.not.be.textInDOM
+            '#wpt-test-form input[type=submit]'.should.be.inDOM
+        })
+    })
+
+    it('should show first result after choosing first question and answer', function() {
+        casper.then(function() {
+            this.clickLabel('Yes', '*[@id="wpt-test-form"]/*[1]/*//label')
+            this.fill('form#wpt-test-form', {}, true)
+        }).waitForUrl(/test.+[a-z0-9]+[a-f0-9]{32}/, function() {
+            'Fatal'.should.not.be.textInDOM
+            'Results'.should.be.textInDOM
+            'Choleric'.should.be.textInDOM
+        })
+    })
+
+})
