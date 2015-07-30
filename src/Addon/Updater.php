21066340713d17b6ea0e6268bd6486dce277c1e8
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-07-31 01:23:44 +0600

    Document external Plugin Update Checker usage

--- src/Addon/Updater.php
+++ src/Addon/Updater.php
@@ -12,6 +12,7 @@ class WpTesting_Addon_Updater
     }
 
     /**
+     * @uses PucFactory
      * @return WpTesting_Addon_Updater
      */
     public function add(WpTesting_Addon_IAddon $addon)
@@ -22,6 +23,16 @@ class WpTesting_Addon_Updater
         $pluginFile     = $root . DIRECTORY_SEPARATOR . $slug . '.php';
         $checkEachHours = 1;
 
+        /**
+         * Uses external library named "Plugin Update Checker" for the purposes of updating paid addons that hosted at
+         * http://apsiholog.ru/addons/
+         *
+         * Updates happens only in admin area and only when addon registered.
+         * So it's not touches in any ways usual plugin users.
+         *
+         * @since 2015-07-31
+         * @author Ustimenko Alexander
+         */
         $checker = PucFactory::buildUpdateChecker(
             $metadataUrl,
             $pluginFile,
