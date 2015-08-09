d1de1f2ced67b9906c3847a273013b13eb357c68
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-08-09 16:31:01 +0600

    Use semantic headers in respondents results as of WP 4.3
    * According to
    https://make.wordpress.org/core/2015/07/31/headings-in-admin-screens-change-in-wordpress-4-3/

--- src/Template/Passing/Browser/view-all.php
+++ src/Template/Passing/Browser/view-all.php
@@ -2,10 +2,15 @@
 /* @var $page integer */
 /* @var $title string */
 /* @var $table WpTesting_Widget_PassingTable */
+/* @var $isSemanticHeaders boolean */
 ?>
 <div class="wrap">
 
+<?php if ($isSemanticHeaders): ?>
+    <h1><?php echo $title ?></h1>
+<?php else: ?>
     <h2><?php echo $title ?></h2>
+<?php endif ?>
 
     <?php $table->views() ?>
 
