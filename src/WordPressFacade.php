e3ebf69c417bf808d724c07df4861914a80f4e93
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-08-30 11:51:46 +0600

    Improve addons resource/hooks integration
    * Respect current class main "namespace" in resource prefixes
    * Add knowledge about admin/public to wp facade
    * Simplify abstract parent creation from constructor
    * Add options to wp facade

--- src/WordPressFacade.php
+++ src/WordPressFacade.php
@@ -1009,6 +1009,47 @@ class WpTesting_WordPressFacade implements WpTesting_Addon_IWordPressFacade
     }
 
     /**
+     * Retrieve option value based on name of option.
+     *
+     * If the option does not exist or does not have a value, then the return value
+     * will be false. This is useful to check whether you need to install an option
+     * and is commonly used during installation of plugin options and to test
+     * whether upgrading is required.
+     *
+     * If the option was serialized then it will be unserialized when it is returned.
+     *
+     * @since 1.5.0
+     *
+     * @param string $option  Name of option to retrieve. Expected to not be SQL-escaped.
+     * @param mixed  $default Optional. Default value to return if the option does not exist.
+     * @return mixed Value set for the option.
+     */
+    public function getOption($option, $default = false)
+    {
+        return get_option($option, $default);
+    }
+
+    /**
+     * Update the value of an option that was already added.
+     *
+     * You do not need to serialize values. If the value needs to be serialized, then
+     * it will be serialized before it is inserted into the database. Remember,
+     * resources can not be serialized or added as an option.
+     *
+     * If the option does not exist, then the option will be added with the option value
+     *
+     * @since 1.0.0
+     *
+     * @param string      $option   Option name. Expected to not be SQL-escaped.
+     * @param mixed       $value    Option value. Must be serializable if non-scalar. Expected to not be SQL-escaped.
+     * @return bool False if value was not updated and true if value was updated.
+     */
+    public function updateOption($option, $value)
+    {
+        return update_option($option, $value);
+    }
+
+    /**
      * Saves option for number of rows when listing posts, pages, comments, etc.
      *
      * @since 2.8.0
