5098c88e5b860ac669ef78754202f22ad3f9eb1d
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-09-27 16:50:18 +0600

    * Remove useless method

--- src/Model/Shortcode/TestFirstPage.php
+++ src/Model/Shortcode/TestFirstPage.php
@@ -4,11 +4,4 @@ class WpTesting_Model_Shortcode_TestFirstPage extends WpTesting_Model_Shortcode_
 {
 
     const NAME = 'wpt_test_first_page';
-
-    public function getDataForTemplate(WpTesting_Facade_IORM $ormAware)
-    {
-        return array_merge(parent::getDataForTemplate($ormAware), array(
-            'content' => null,
-        ));
-    }
 }
