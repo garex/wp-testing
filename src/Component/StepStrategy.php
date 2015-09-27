8ad5bc094ec63537edf4828f460c80c76c87d774
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2015-09-27 12:13:09 +0600

    * Make first 1step always current so we dont' need any checks

--- src/Component/StepStrategy.php
+++ src/Component/StepStrategy.php
@@ -147,6 +147,10 @@ abstract class WpTesting_Component_StepStrategy
      */
     protected function addStep(WpTesting_Model_Step $step, $isCurrent = true)
     {
+        // First step always current
+        if (empty($this->steps)) {
+            $isCurrent = true;
+        }
         $this->steps[] = $step;
         if ($isCurrent) {
             $this->currentStep = $step;
@@ -170,14 +174,11 @@ abstract class WpTesting_Component_StepStrategy
      */
     protected function getSteps()
     {
-        if (is_null($this->currentStep)) {
+        if (empty($this->steps)) {
             $this->fillSteps();
             if (!count($this->steps)) {
                 $emptyQuestions = fRecordSet::buildFromArray('WpTesting_Model_Question', array());
                 $this->addStep(new WpTesting_Model_Step('', $emptyQuestions));
-            }
-            if (is_null($this->currentStep)) {
-                $this->currentStep = reset($this->steps);
             }
             $this->setupTotalsAndNumbers();
         }
