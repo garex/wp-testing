fafc40965415c03ec0d33ea4de4c5e4c5c51b569
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2016-02-19 23:13:07 +0600

    Introduce maximize for questions and formulas
    * Use settings in maximize and locale keys
    * Set max-height for maximizable to 40em
    * Add latest and old WP maximize icons
    * Place icon on the right

---
+++ js/test-edit-maximize-metaboxes.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+    $('#wpt_edit_questions,#wpt_edit_formulas').wptMaximize({
+        maximizeLabel: Wpt.locale.maximize,
+        minimizeLabel: Wpt.locale.minimize
+    });
+});
