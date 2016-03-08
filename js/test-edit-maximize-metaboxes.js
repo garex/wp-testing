f3a8f26cb6a9a6ddee46a924792fc5b6f3ed97ea
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2016-03-08 09:08:40 +0600

    Maximize on new metaboxes

--- js/test-edit-maximize-metaboxes.js
+++ js/test-edit-maximize-metaboxes.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-    $('#wpt_edit_questions,#wpt_edit_formulas').wptMaximize({
+    $('#wpt_edit_questions_answers,#wpt_edit_scores,#wpt_edit_formulas').wptMaximize({
         maximizeLabel: Wpt.locale.maximize,
         minimizeLabel: Wpt.locale.minimize
     });
