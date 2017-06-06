d41e8b8b8705975427c98566f7135d95044f69e7
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2017-06-06 23:41:59 +0700

    Avoid unicode escape
    * Extract json component for backward compatibility

--- src/Template/Abstract/js-data.php
+++ src/Template/Abstract/js-data.php
@@ -1,14 +1,14 @@
-<?php /* @var array $Wpt */ ?>
+<?php /* @var array $Wpt */ /* @var WpTesting_Component_Json $j */ ?>
 <script type="text/javascript">
 /* <![CDATA[ */
 var Wpt = Wpt || {};
 <?php foreach ($Wpt as $key => $value): ?>
 <?php if (!is_array($value)): ?>
-Wpt[<?php echo json_encode($key) ?>] = <?php echo json_encode($value) ?>;
+Wpt[<?php echo $j->encode($key) ?>] = <?php echo $j->encode($value) ?>;
 <?php else: ?>
-Wpt[<?php echo json_encode($key) ?>] = Wpt[<?php echo json_encode($key) ?>] || <?php if(is_string(key($value))): ?>{}<?php else: ?>[]<?php endif ?>;
+Wpt[<?php echo $j->encode($key) ?>] = Wpt[<?php echo $j->encode($key) ?>] || <?php if(is_string(key($value))): ?>{}<?php else: ?>[]<?php endif ?>;
 <?php foreach ($value as $subKey => $subValue): ?>
-Wpt[<?php echo json_encode($key) ?>][<?php echo json_encode($subKey) ?>] = <?php echo json_encode($subValue) ?>;
+Wpt[<?php echo $j->encode($key) ?>][<?php echo $j->encode($subKey) ?>] = <?php echo $j->encode($subValue) ?>;
 <?php endforeach ?>
 <?php endif ?>
 <?php endforeach ?>
