85b6e21d82be0927f7c76e08aa098b393724cda3
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2018-06-24 16:32:27 +0700

    Extract facade factories into WpTesting_Tests_TestCase

---
+++ tests/phpunit/TestCase.php
@@ -0,0 +1,45 @@
+<?php
+
+abstract class WpTesting_Tests_TestCase extends PHPUnit_Framework_TestCase
+{
+    private static $wpFacade;
+
+    private static $facade;
+
+    public static function setUpBeforeClass()
+    {
+        self::$wpFacade = self::createWordPressFacade();
+        self::$facade = self::createFacade(self::$wpFacade);
+    }
+
+    /**
+     * @return WpTesting_WordPressFacade
+     */
+    protected function getWpFacade()
+    {
+        return self::$wpFacade;
+    }
+
+    /**
+     * @return WpTesting_Facade
+     */
+    protected function getFacade()
+    {
+        return self::$facade;
+    }
+
+    private static function createWordPressFacade()
+    {
+        $pluginFile = realpath(dirname(__FILE__) . '/../../wp-testing.php');
+        $migration = require dirname(__FILE__) . '/../../db/ruckusing.conf.php';
+
+        return new WpTesting_Mock_WordPressFacade($pluginFile, $migration['db']['development']);
+    }
+
+    private static function createFacade(WpTesting_WordPressFacade $wp)
+    {
+        fORMDatabase::reset();
+
+        return new WpTesting_Mock_Facade($wp);
+    }
+}
