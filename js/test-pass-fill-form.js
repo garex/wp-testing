0a8a7a48c7a951b5c6903859703ce626c15c3a50
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2020-10-11 18:46:23 +0700

    add es5

--- js/test-pass-fill-form.js
+++ js/test-pass-fill-form.js
@@ -20,7 +20,7 @@ Wpt.initWebshim = function(baseUrl) {
             }
         }
     });
-    webshims.polyfill('forms forms-ext');
+    webshims.polyfill('forms forms-ext es5');
 };
 
 if (Wpt.webshimBaseurl) {
