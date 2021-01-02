0f38400db363b9eea74bc58223c60c616188f8b1
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2021-01-03 01:07:25 +0700

    test wp is installed

--- tools/docker/mocha2/test/0-wordpress.js
+++ tools/docker/mocha2/test/0-wordpress.js
@@ -1,7 +1,36 @@
 require('chai').should();
+const puppeteer = require('puppeteer-core');
 
 describe('WordPress', () => {
-  it('should be installed', () => {
+  it('should be installed', async () => {
+    const browser = await puppeteer.connect({
+      browserURL: 'http://chrome.localhost:9222/',
+      defaultViewport: {
+        width: 1280,
+        height: 720,
+        hasTouch: false,
+        isMobile: false,
+        isLandscape: true,
+      },
+    });
+
+    const pages = await browser.pages();
+    pages.forEach(async (page) => {
+      await page.close();
+    });
+
     [1, 2, 3].indexOf(4).should.be.equal(-1);
+
+    const page = await browser.newPage();
+
+    await page.goto('http://wpt.docker/');
+
+    const title = await page.title();
+
+    title.should.contain('wpti');
+
+    page.close();
+
+    browser.disconnect();
   });
 });
