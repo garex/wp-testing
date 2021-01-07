74e1ef4239a491c1f1f33a010cf4d8e7bef10b33
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2021-01-07 16:38:39 +0700

    activate plugin!

--- tools/docker/mocha2/test/1-plugin-activation.js
+++ tools/docker/mocha2/test/1-plugin-activation.js
@@ -1,12 +1,30 @@
 require('chai').should();
+const puppeteer = require('puppeteer-core'); // eslint-disable-line no-unused-vars
 const _ = require('./_');
 
 describe('Plugin activation', () => {
+  /** @type puppeteer.Page */
+  let page;
+
   it('should open plugins page', async () => {
-    const page = await _.adminPage();
+    page = await _.adminPage();
 
     await page.goto('http://wpt.docker/wp-admin/plugins.php');
 
     (await page.$eval('body', (body) => body.innerText)).should.contains('Wp-testing');
+  });
+
+  it('should activate main plugin and others', async () => {
+    await page.click('#cb input');
+    await page.select('.wrap form select:first-of-type', 'activate-selected');
+
+    await Promise.all([
+      page.click('#doaction'),
+      page.waitForNavigation(),
+      page.waitForResponse((response) => response.url().includes('activate')),
+    ]);
+
+    const el = await page.$('#wp-testing .deactivate a,[data-slug=wp-testing] .deactivate a');
+    el.should.be.not.null;
   });
 });
--- tools/docker/mocha2/test/_.js
+++ tools/docker/mocha2/test/_.js
@@ -1,20 +1,23 @@
 require('chai').should();
 const puppeteer = require('puppeteer-core');
 
+/** @type puppeteer.Browser */
 let browser;
 
 module.exports.mochaHooks = {
-  afterEach: () => {
+  afterAll: () => {
     browser.disconnect();
   },
 };
 
 module.exports.page = async () => {
-  browser = await puppeteer.connect({
-    browserURL: 'http://localhost:9222/',
-    defaultViewport: null,
-    slowMo: 10,
-  });
+  if (typeof browser === 'undefined') {
+    browser = await puppeteer.connect({
+      browserURL: 'http://localhost:9222/',
+      defaultViewport: null,
+      //    slowMo: 10,
+    });
+  }
 
   // (await browser.pages()).forEach(async (page) => { await page.close(); });
 
@@ -29,12 +32,10 @@ module.exports.adminPage = async () => {
   await page.click('input[name=log]', { clickCount: 3 });
   await page.type('input[name=log]', 'wpti');
   await page.type('input[name=pwd]', 'wpti');
-
-  await page.evaluate(() => { debugger; }); // eslint-disable-line no-debugger
-
-  await page.click('input[type=submit]');
-
-  //  await page.waitForNavigation();
+  await Promise.all([
+    page.click('input[type=submit]'),
+    page.waitForNavigation(),
+  ]);
 
   (await page.content()).should.contains('Dashboard');
 
